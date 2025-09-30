2b06c22c7172dd058f2f7e1ea3ee78f0304d5b5e
Maria Eduarda Amaral <mariaeduardaamaralsb@gmail.com>
2025-09-29 22:56:15 -0300

    Atualiza .gitignore para ignorar binários e dependências

--- src/DentistrySched.Application/ISlotService.cs
+++
@@ -1,8 +0,0 @@
-﻿using DentistrySched.Application.DTOs;
-
-namespace DentistrySched.Application;
-
-public interface ISlotService
-{
-    Task<IReadOnlyList<SlotDto>> GerarSlotsAsync(DateOnly dia, Guid dentistaId, Guid procedimentoId, CancellationToken ct);
-}
